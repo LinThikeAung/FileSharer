8f04a789583effbd134c703fee01effeedb2290e
thantzinaungtgo <thantzinaung@dkmads.com>
2023-06-02 09:27:16 +0630

    Changes Backend Files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,7 +33,7 @@
         <div class="col-md-6 grid-margin stretch-card">
             <div class="card tale-bg">
                 <div class="card-people mt-auto">
-                    <img src="{{ asset('admin/images/dashboard/people.svg') }}" alt="people">
+                    <img src="{{ asset('backend/images/dashboard/people.svg') }}" alt="people">
                     <div class="weather-info">
                         <div class="d-flex">
                             <div>
@@ -622,7 +622,7 @@
                     <ul class="icon-data-list">
                         <li>
                             <div class="d-flex">
-                                <img src="{{ asset('admin/images/faces/face1.jpg') }}" alt="user">
+                                <img src="{{ asset('backend/images/faces/face1.jpg') }}" alt="user">
                                 <div>
                                     <p class="text-info mb-1">Isabella Becker</p>
                                     <p class="mb-0">Sales dashboard have been created</p>
@@ -632,7 +632,7 @@
                         </li>
                         <li>
                             <div class="d-flex">
-                                <img src="{{ asset('admin/images/faces/face2.jpg') }}" alt="user">
+                                <img src="{{ asset('backend/images/faces/face2.jpg') }}" alt="user">
                                 <div>
                                     <p class="text-info mb-1">Adam Warren</p>
                                     <p class="mb-0">You have done a great job #TW111</p>
@@ -642,7 +642,7 @@
                         </li>
                         <li>
                             <div class="d-flex">
-                                <img src="{{ asset('admin/images/faces/face3.jpg') }}" alt="user">
+                                <img src="{{ asset('backend/images/faces/face3.jpg') }}" alt="user">
                                 <div>
                                     <p class="text-info mb-1">Leonard Thornton</p>
                                     <p class="mb-0">Sales dashboard have been created</p>
@@ -652,7 +652,7 @@
                         </li>
                         <li>
                             <div class="d-flex">
-                                <img src="{{ asset('admin/images/faces/face4.jpg') }}" alt="user">
+                                <img src="{{ asset('backend/images/faces/face4.jpg') }}" alt="user">
                                 <div>
                                     <p class="text-info mb-1">George Morrison</p>
                                     <p class="mb-0">Sales dashboard have been created</p>
@@ -662,7 +662,7 @@
                         </li>
                         <li>
                             <div class="d-flex">
-                                <img src="{{ asset('admin/images/faces/face5.jpg') }}" alt="user">
+                                <img src="{{ asset('backend/images/faces/face5.jpg') }}" alt="user">
                                 <div>
                                     <p class="text-info mb-1">Ryan Cortez</p>
                                     <p class="mb-0">Herbs are fun and easy to grow.</p>
