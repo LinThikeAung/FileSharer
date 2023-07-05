46aec54b20baa9bd753984feb124fa5ecf977988
lwinkokointern1 <dkmadsintern1@gmail.com>
2023-07-05 09:21:20 +0630

    add_evn_and_sql_to_lwinkoko

--- custom/main_folders.sql
+++ custom/main_folders.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 04, 2023 at 06:36 AM
+-- Generation Time: Jul 05, 2023 at 04:50 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -24,14 +24,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `files`
+-- Table structure for table `main_folders`
 --
 
-CREATE TABLE `files` (
+CREATE TABLE `main_folders` (
   `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL,
-  `main_folder_id` bigint(20) UNSIGNED DEFAULT NULL,
-  `sub_folder_id` bigint(20) UNSIGNED DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -41,21 +40,20 @@ CREATE TABLE `files` (
 --
 
 --
--- Indexes for table `files`
+-- Indexes for table `main_folders`
 --
-ALTER TABLE `files`
+ALTER TABLE `main_folders`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `files_main_folder_id_foreign` (`main_folder_id`),
-  ADD KEY `files_sub_folder_id_foreign` (`sub_folder_id`);
+  ADD KEY `main_folders_user_id_foreign` (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `files`
+-- AUTO_INCREMENT for table `main_folders`
 --
-ALTER TABLE `files`
+ALTER TABLE `main_folders`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
@@ -63,11 +61,10 @@ ALTER TABLE `files`
 --
 
 --
--- Constraints for table `files`
+-- Constraints for table `main_folders`
 --
-ALTER TABLE `files`
-  ADD CONSTRAINT `files_main_folder_id_foreign` FOREIGN KEY (`main_folder_id`) REFERENCES `main_folders` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `files_sub_folder_id_foreign` FOREIGN KEY (`sub_folder_id`) REFERENCES `sub_folders` (`id`) ON DELETE CASCADE;
+ALTER TABLE `main_folders`
+  ADD CONSTRAINT `main_folders_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
