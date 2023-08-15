691b8175fb4e544083e90cae9d2fbb57bf52707d
lwinkokointern1 <dkmadsintern1@gmail.com>
2023-08-15 11:22:16 +0630

    change_design_in_hardisk_usage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,29 +26,29 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         View::composer('*', function ($view) {
-            $data =null ;
+            $data = null ;
             if(auth()->check()){             
-                $disktotal  = disk_total_space('/media/dkmads-upload/');
-                $disktotalsize = $disktotal / 1073741824;
-                $diskfree   = disk_free_space('/media/dkmads-upload/');
-                $used       = $disktotal - $diskfree;
-                $diskusedsize = $used / 1073741824;
-                $diskuse1   = round(100 - (($diskusedsize / $disktotalsize) * 100));
-                $diskuse = round(100 - ($diskuse1)) . '%';
-                $disk_total = disk_total_space('/media/dkmads-upload2/');
+                $disktotal       = disk_total_space('/media/dkmads-upload/');
+                $disktotalsize   = $disktotal / 1073741824;
+                $diskfree        = disk_free_space('/media/dkmads-upload/');
+                $used            = $disktotal - $diskfree;
+                $diskusedsize    = $used / 1073741824;
+                $diskuse1        = round(100 - (($diskusedsize / $disktotalsize) * 100));
+                $diskuse         = round(100 - ($diskuse1)) . '%';
+                $disk_total      = disk_total_space('/media/dkmads-upload2/');
                 $disk_total_size = $disk_total / 1073741824;
-                $disk_free  = disk_free_space('/media/dkmads-upload2/');  
-                $used2      = $disk_total - $disk_free;
-                $disk_used_size = $used2 / 1073741824;
-                $diskuse2   = round(100 - (($disk_used_size / $disk_total_size) * 100));
-                $diskuse2 = round(100 - ($diskuse2)) . '%';
+                $disk_free       = disk_free_space('/media/dkmads-upload2/');  
+                $used2           = $disk_total - $disk_free;
+                $disk_used_size  = $used2 / 1073741824;
+                $diskuse2        = round(100 - (($disk_used_size / $disk_total_size) * 100));
+                $diskuse2        = round(100 - ($diskuse2)) . '%';
                 $data = [
                     'disktotal'  => $this->formatFileSize($disktotal),
                     'used'       => $this->formatFileSize($used),
                     'diskuse'    => $diskuse,
                     'disk_total' => $this->formatFileSize($disk_total),
                     'used2'      => $this->formatFileSize($used2),
-                    'diskuse2'    => $diskuse2,
+                    'diskuse2'   => $diskuse2,
                 ];
                 $view->with('data',$data);
             }
