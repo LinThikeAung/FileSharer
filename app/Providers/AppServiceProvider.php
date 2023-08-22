20bf3962b66bb209c1c5adce4eed08359661bdd2
thantzinaungtgo <thantzinaung@dkmads.com>
2023-08-22 15:26:59 +0630

    Update Code

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,30 +25,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::composer('*', function ($view) {
-            $data = null ;
-            if(auth()->check()){      
-                //Disk 1       
-                $output = shell_exec('df -h /media/dkmads-upload/');
-                $lines  = explode("\n", $output);
-                $parts  = preg_split('/\s+/', $lines[1]);
+        // View::composer('*', function ($view) {
+        //     $data = null ;
+        //     if(auth()->check()){      
+        //         //Disk 1       
+        //         $output = shell_exec('df -h /media/dkmads-upload/');
+        //         $lines  = explode("\n", $output);
+        //         $parts  = preg_split('/\s+/', $lines[1]);
 
-                //Disk 2
-                $output2 =  shell_exec('df -h /media/dkmads-upload2/');
-                $lines2  = explode("\n", $output2);
-                $parts2  = preg_split('/\s+/', $lines2[1]);
-                $data = [
-                    'disktotal1'  => $parts[1],
-                    'used1'       => $parts[2],
-                    'available1'  => $parts[3],
-                    'percent1'    => $parts[4],
-                    'disktotal2'  => $parts2[1],
-                    'used2'       => $parts2[2],
-                    'available2'  => $parts2[3],
-                    'percent2'    => $parts2[4],
-                ];
-                $view->with('data',$data);
-            }
-        });
+        //         //Disk 2
+        //         $output2 =  shell_exec('df -h /media/dkmads-upload2/');
+        //         $lines2  = explode("\n", $output2);
+        //         $parts2  = preg_split('/\s+/', $lines2[1]);
+        //         $data = [
+        //             'disktotal1'  => $parts[1],
+        //             'used1'       => $parts[2],
+        //             'available1'  => $parts[3],
+        //             'percent1'    => $parts[4],
+        //             'disktotal2'  => $parts2[1],
+        //             'used2'       => $parts2[2],
+        //             'available2'  => $parts2[3],
+        //             'percent2'    => $parts2[4],
+        //         ];
+        //         $view->with('data',$data);
+        //     }
+        // });
     }
 }
